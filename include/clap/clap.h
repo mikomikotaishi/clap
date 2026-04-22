387e54d667bdf4ac25057a5e5ad6fb5d4bc48773
Nathan Baggs <hello@nathanbaggs.com>
2026-04-22 19:40:59 +0100

    Simple string parse

--- include/clap/clap.h
+++ include/clap/clap.h
@@ -1,11 +1,82 @@
 #pragma once
 
+#include <format>
+#include <meta>
+#include <ranges>
+#include <span>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <vector>
+
 namespace clap
 {
 
-inline auto simple() -> int
+class Exception : public std::runtime_error
 {
-    return 42;
+  public:
+    template <class... Args>
+    constexpr Exception(std::format_string<Args...> msg, Args &&...args)
+        : std::runtime_error{std::format(msg, std::forward<Args>(args)...)}
+    {
+    }
+};
+
+namespace impl
+{
+
+constexpr auto convert_raw_args(const int argc, char const *const *argv) -> std::vector<std::string_view> //
+    pre(argc > 1)                                                                                         //
+    post(r : std::ranges::size(r) == argc - 1zu)
+{
+    return std::span{argv, argv + argc} |                                                 //
+           std::views::drop(1) |                                                          //
+           std::views::transform([](const auto *arg) { return std::string_view{arg}; }) | //
+           std::ranges::to<std::vector>();
+}
+
+constexpr auto find_arg(std::span<const std::string_view> args, std::string_view arg) -> std::string_view
+{
+    const auto arg_iter = std::ranges::find(args, arg);
+    if (arg_iter == std::ranges::cend(args))
+    {
+        throw Exception("missing argument: {}", arg);
+    }
+
+    if (arg_iter == std::ranges::cend(args) - 1u)
+    {
+        throw Exception("missing value for argument: {}", arg);
+    }
+
+    return *(arg_iter + 1u);
+}
+
+constexpr auto format_member_as_arg(std::string_view member_name) -> std::string
+{
+    return std::format("--{}", member_name);
+}
+
+}
+
+template <class T>
+    requires std::is_default_constructible_v<T>
+constexpr auto parse(int argc, char const *const *argv) -> T //
+    pre(argc > 1)
+{
+    auto args = impl::convert_raw_args(argc, argv);
+    constexpr auto ctx = std::meta::access_context::current();
+
+    auto res = T{};
+
+    template for (constexpr auto member : std::define_static_array(std::meta::nonstatic_data_members_of(^^T, ctx)))
+    {
+        const auto arg_str = impl::format_member_as_arg(std::meta::identifier_of(member));
+        const auto arg = impl::find_arg(args, arg_str);
+        res.[:member:] = arg;
+    }
+
+    return res;
 }
 
 }
--- tests/clap_tests.cpp
+++ tests/clap_tests.cpp
@@ -1,8 +1,49 @@
+#include <contracts>
+#include <iostream>
+#include <print>
+#include <stacktrace>
+#include <string>
+
 #include <gtest/gtest.h>
 
 #include "clap/clap.h"
 
-TEST(clap, simple)
+void handle_contract_violation(std::contracts::contract_violation cv);
+void handle_contract_violation(std::contracts::contract_violation cv)
 {
-    ASSERT_EQ(clap::simple(), 42);
+    std::println(std::cerr, "{}:{} {}", cv.location().file_name(), cv.location().line(), cv.comment());
+    std::println(std::cerr, "{}", std::stacktrace::current());
+}
+
+namespace
+{
+struct Args
+{
+    std::string colour;
+    auto operator==(const Args &) const -> bool = default;
+};
+}
+
+TEST(clap, single_string)
+{
+    const auto expected = Args{.colour = "red"};
+    const auto args = std::vector{"./program", "--colour", "red"};
+
+    ASSERT_EQ(clap::parse<Args>(args.size(), args.data()), expected);
+}
+
+TEST(clap, missing_arg)
+{
+    const auto expected = Args{.colour = "red"};
+    const auto args = std::vector{"./program", "red"};
+
+    ASSERT_THROW(clap::parse<Args>(args.size(), args.data()), clap::Exception);
+}
+
+TEST(clap, missing_arg_value)
+{
+    const auto expected = Args{.colour = "red"};
+    const auto args = std::vector{"./program", "--colour"};
+
+    ASSERT_THROW(clap::parse<Args>(args.size(), args.data()), clap::Exception);
 }
